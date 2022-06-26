ad33f55f6054d2780bb748e42e73d94e1be57d82
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-06-26 15:10:10 +0300

    task_4.py changed

--- task_4.py
+++ task_4.py
@@ -1,10 +1,9 @@
 string = input('Введите строку: ').lower()  # use lower() - convert to lowercase
 symbol = input('Введите символ: ').lower()
 
-index1 = string.index(symbol)
-index2 = string.rindex(symbol)
-
 if symbol in string:
+    index1 = string.index(symbol)
+    index2 = string.rindex(symbol)
     print('Индекс первого вхождения символа: ', index1)
     print('Индекс последнего вхождения символа: ', index2)
 else:
